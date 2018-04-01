92b3c17bbb166cde9ed416c2c7ae1a897c963fa5
MFSgit <mohitwin55@outlook.com>
2018-04-01 07:21:51 +0530

    first commit

---
+++ adamaas_worksp/ip_BotForCv1.3.cpp
@@ -0,0 +1,163 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string.h>
+
+using namespace cv;
+using namespace std;
+
+int fd;
+int color_threshold = 30, location_threshold = 40, angle_threshold = 2;
+int h1 = 0, s1 = 63, v1 = 52;
+int h2 = 196, s2 = 102, v2 = 189;
+char serialPortFilename[] = "/dev/serial/by-id/usb-Arduino_Srl_Arduino_Uno_55431313038351D0C0D1-if00";
+
+Mat src, src_gray;
+int thresh = 100, max_thresh = 255;
+RNG rng(12345);
+
+void thresh_callback(int, void *);
+/*
+ * Controls to move the arduino:
+ * w := forward
+ * s := backward
+ * d := right
+ * a := left
+ */
+void sendCommand(const char *command) {
+	char send=*command;
+	//write(fd, command, 1);
+	FILE *serport =fopen("/dev/serial/by-id/usb-Arduino_Srl_Arduino_Uno_55431313038351D0C0D1-if00","w");
+	fprintf(serport,"%c",send);
+	fclose(serport);
+}
+
+int connectArduino(const char *port) {
+	fd = open(port, O_RDWR | O_NOCTTY);
+	printf("Started the connection to arduino\n");
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	VideoCapture cap(1);
+	if (!cap.isOpened()) {
+		printf("ERROR!\n");
+		return -1;
+	}
+	namedWindow("Trackbars");
+	createTrackbar("h1", "Trackbars", &h1, 255);
+	createTrackbar("s1", "Trackbars", &s1, 255);
+	createTrackbar("v1", "Trackbars", &v1, 255);
+	createTrackbar("h2", "Trackbars", &h2, 255);
+	createTrackbar("s2", "Trackbars", &s2, 255);
+	createTrackbar("v2", "Trackbars", &v2, 255);
+	connectArduino(argv[1]);
+
+	while (1) {
+		Mat imageHsv;
+		Mat finalImg;
+		const char *source_window = "Source";
+
+		cap >> src;
+		cvtColor(src, imageHsv, CV_BGR2HSV);
+		inRange(imageHsv, Scalar(h1, s1, v1), Scalar(h2, v2, s2), src_gray);
+		imshow("ragna", src_gray);
+
+		blur(src_gray, src_gray, Size(3, 3));
+		namedWindow(source_window, CV_WINDOW_AUTOSIZE);
+		imshow(source_window, src);
+
+		createTrackbar(" Threshold: ", "Source", &thresh, max_thresh,
+					   thresh_callback);
+		thresh_callback(0, NULL);
+
+		waitKey(27);
+	}
+	close(fd);
+
+	return 0;
+}
+
+void thresh_callback(int a, void *b)
+{
+	Mat threshold_output;
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	Point center_rect;
+	float area = INT_MAX;
+	int img_x, obj_x;
+
+	/* Detect edges using Threshold */
+	threshold(src_gray, threshold_output, thresh, 255, THRESH_BINARY);
+
+	/* Find contours */
+	findContours(threshold_output, contours, hierarchy, CV_RETR_TREE,
+				 CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+        //imshow("threshold_output", threshold_output);
+
+	/* Approximate contours to polygons + get bounding rects and circles */
+	vector<vector<Point> > contours_poly(contours.size());
+	vector<Rect> boundRect;
+	for (int i = 0; i < contours.size(); i++) {
+		if (contourArea(contours[i]) > /* 10000 */ 4500 &&
+			contourArea(contours[i]) < 50000) {
+				approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
+				boundRect.push_back(boundingRect(Mat(contours_poly[i])));
+				area = contourArea(contours[i]);
+		}
+	}
+	if (boundRect.size() == 0) {
+		/* Send character 's' to device */
+		sendCommand("S");
+	}
+
+	/* Draw polygon contour + bounding rects + circles */
+	Mat drawing = Mat::zeros(threshold_output.size(), CV_8UC3);
+	for (int i = 0; i < boundRect.size(); i++) {
+		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255),
+							  rng.uniform(0, 255));
+		rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 2,
+				  8, 0);
+		center_rect = (boundRect[i].tl() + boundRect[i].br());
+		center_rect.x /= 2;
+		center_rect.y /= 2;
+		circle(drawing, center_rect, 10, Scalar(255, 0, 0), CV_FILLED, 8, 0);
+	}
+
+	/* Main logic */
+	img_x = src.cols / 2;
+	obj_x = center_rect.x;
+	if (img_x - 100 > obj_x && 0 < obj_x) {
+		cout << "Go left" << endl;
+		sendCommand("A");
+	}
+	else if (img_x + 100 < obj_x) {
+		cout << "Go right" << endl;
+		sendCommand("D");
+	}
+	else {
+		if (area < 50000) {
+			cout << "Straight" << endl;
+                        printf("area = %f ", area);
+			sendCommand("W");
+		}
+		else {
+			cout << "Stopping" << endl;
+			sendCommand("S");
+			return ;
+		}
+	}
+
+	/* Show in a window */
+	namedWindow("Contours", CV_WINDOW_AUTOSIZE);
+	imshow("Contours", drawing);
+	waitKey(150);
+}
---
+++ adamaas_worksp/ip_Botv1.3.py
@@ -0,0 +1,182 @@
+#____________________________________________________________________________________________________________________________________________________
+
+from pyfirmata import Arduino, util
+import time 
+
+import numpy as np
+import argparse
+import cv2
+import math
+
+#___________________________________________________________Setup for py firmata_______________________________________________________________
+board = Arduino("/dev/ttyACM0") # "/dev/ttyACM0" is the communication port for the arduino usb , it may vary , in Windows use "COM0" 
+
+iterator = util.Iterator(board) 
+iterator.start()
+
+
+LeftM1 = board.get_pin('d:5:p')# 'd' stands for digital '5' is the pin no. on arduino 'p' is for pwd singal 
+LeftM2 = board.get_pin('d:6:p')
+RightM1 = board.get_pin('d:10:p')
+RightM2 = board.get_pin('d:11:p')
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------------
+def constrn(value,Llim,Ulim):# Llim is Lower bound,Ulim is Uower bound
+	if (value>Ulim):
+		value=Ulim
+	if (value<Llim):
+		value=Llim
+	else : 
+		value = value
+	return value	
+	
+	
+  
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+    help = "path to the (optional) video file")
+args = vars(ap.parse_args())
+ 
+# define the upper and lower boundaries of the HSV pixel
+# intensities to be considered for red color
+
+
+lower = np.array([0, 100, 100], dtype = "uint8")
+upper = np.array([255, 255, 255], dtype = "uint8")
+
+
+# if a video path was not supplied, grab the reference
+# to the gray
+if not args.get("video", False):
+     camera = cv2.VideoCapture(0)
+ 
+# otherwise, load the video
+else:
+     camera = cv2.VideoCapture(args["video"])
+# keep looping over the frames in the video
+#_____________________________________________________Main loop for execution__________________________________________________________________________
+while True:
+     # grab the current frame
+     (grabbed, frame) = camera.read()
+ 
+     # if we are viewing a video and we did not grab a
+     # frame, then we have reached the end of the video
+     if args.get("video") and not grabbed:
+         break
+ 
+     # resize the frame, convert it to the HSV color space,         
+     # and determine the HSV pixel intensities that fall into
+     # the speicifed upper and lower boundaries
+
+     frame = cv2.resize(frame,None,fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
+     
+
+     converted = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+     objectMask = cv2.inRange(converted, lower, upper)
+ 
+     # apply a series of erosions and dilations to the mask
+     # using an elliptical kernel
+     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+     objectMask = cv2.erode(objectMask, kernel, iterations = 2)
+     objectMask = cv2.dilate(objectMask, kernel, iterations = 2)
+ 
+     # blur the mask to help remove noise, then apply the
+     # mask to the frame
+     objectMask = cv2.GaussianBlur(objectMask, (3, 3), 0)
+     objectd = cv2.bitwise_and(frame, frame, mask = objectMask)
+     
+     # converting to gray scale
+     gray = cv2.cvtColor(objectd, cv2.COLOR_BGR2GRAY)
+     height, width = gray.shape
+
+     linePixSum = 0
+     threshold = 5000
+	
+     wall1 = 0
+     wall2 = 0
+     #_______________________________________Algorithm for detection of the centre of the block________________________________________________________
+     for x1  in range(0,width):
+	for y1 in range(0,height):
+ 		linePixSum += gray[y1,(width-x1-1)]
+	if (linePixSum > threshold):
+		cv2.line(gray,(width-x1-1,0),(width-x1-1,height),(255,0,0),2)
+		wall1 = width-x1-1	
+		break	
+	linePixSum = 0	 
+     linePixSum = 0	
+     
+     for x2 in range(0,width):
+	for y2 in range(0,height):
+ 		linePixSum += gray[y2,x2]
+	if (linePixSum > threshold):
+		cv2.line(gray,(x2,0),(x2,height),(255,0,0),2)
+		wall2 = x2				
+		break
+	linePixSum = 0	 
+     linePixSum = 0
+    	
+     
+     #print("The left wall is at  ",wall1," and right wall is at",wall2,"\n")
+     
+     center = (wall1 + wall2)/2
+     pitch = 160-abs(wall1 - wall2)
+     pitch = pitch/2
+     pitch = 1.4*pitch
+      
+     if pitch == 112 :
+        pitch = 0	
+     yaw = center-(width/2)
+     yaw = 0.7*yaw
+     
+     if yaw == -112:
+     	yaw = 0
+     
+     if yaw>-0.2 and yaw <0.2:
+	yaw = 0	
+     #-------------------------------------------------------------------------------------------------------------------------------------------
+     
+     r_motor = (pitch+yaw)/200.0
+     l_motor = (pitch-yaw)/200.0
+     
+     r_motor1 = constrn(r_motor,0.0,1.0)
+     l_motor1 = constrn(l_motor,0.0,1.0)
+     
+     r_motor2 = abs(constrn(r_motor,-1.0,0.0))
+     l_motor2 = abs(constrn(l_motor,-1.0,0.0))
+     
+     # print("Rm1",r_motor1,"Rm2",r_motor2,"Lm1",l_motor1,"Lm2",l_motor2,"\n")
+     
+     print 'RM1 = %(Rm1).2f RM2 = %(Rm2).2f LM1 =  %(Lm1).2f LM2 = %(Lm2).2f ' % {
+     'Rm1': r_motor1,
+     'Rm2': r_motor2,
+     'Lm1': l_motor1,
+     'Lm2': l_motor2,
+     }
+     
+     # commands for motors--------------------------------------------------------- 
+     LeftM1.write(l_motor1)
+     LeftM2.write(l_motor2)
+     RightM1.write(r_motor1)
+     RightM2.write(r_motor2)
+
+     # show the objectd in the image along with the mask
+     cv2.imshow("images", np.hstack([frame, objectd]))
+     cv2.imshow("detection", gray)
+  
+     # if the 'q' key is pressed, stop the loop
+     if cv2.waitKey(1) & 0xFF == ord("q"):
+	  LeftM1.write(0)
+     	  LeftM2.write(0)
+     	  RightM1.write(0)
+     	  RightM2.write(0)
+	  break
+
+   	 	
+
+board.exit()
+# cleanup the camera and close any open windows
+camera.release()
+cv2.destroyAllWindows()
+
